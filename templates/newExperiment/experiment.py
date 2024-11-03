dd5796fdaaff97682efa481e1659b9bbd7ff695b
Roy <royyuan19@gmail.com>
2024-11-03 12:00:01 -0800

    moved code

--- templates/newExperiment/experiment.py
+++ templates/newExperiment/experiment.py
@@ -13,9 +13,127 @@ import torch.optim as optim
 import torch.nn.functional as F
 
 
-from utils.CAModel import CAModel
+#from utils.CAModel import CAModel
+#from utils.utils_vis import SamplePool, to_rgb, get_living_mask, make_seed, make_circle_masks
+#stuff from utils so scientist can edit it
+#utils vis
+class SamplePool:
+    def __init__(self, *, _parent=None, _parent_idx=None, **slots):
+        self._parent = _parent
+        self._parent_idx = _parent_idx
+        self._slot_names = slots.keys()
+        self._size = None
+        for k, v in slots.items():
+            if self._size is None:
+                self._size = len(v)
+            assert self._size == len(v)
+            setattr(self, k, np.asarray(v))
 
-from utils.utils_vis import SamplePool, to_rgb, get_living_mask, make_seed, make_circle_masks
+    def sample(self, n):
+        idx = np.random.choice(self._size, n, False)
+        batch = {k: getattr(self, k)[idx] for k in self._slot_names}
+        batch = SamplePool(**batch, _parent=self, _parent_idx=idx)
+        return batch
+
+    def commit(self):
+        for k in self._slot_names:
+            getattr(self._parent, k)[self._parent_idx] = getattr(self, k)
+
+def to_alpha(x):
+    return np.clip(x[..., 3:4], 0, 0.9999)
+
+def to_rgb(x):
+    # assume rgb premultiplied by alpha
+    rgb, a = x[..., :3], to_alpha(x)
+    return np.clip(1.0-a+rgb, 0, 0.9999)
+
+def get_living_mask(x):
+    return nn.MaxPool2d(3, stride=1, padding=1)(x[:, 3:4, :, :])>0.1
+
+def make_seeds(shape, n_channels, n=1):
+    x = np.zeros([n, shape[0], shape[1], n_channels], np.float32)
+    x[:, shape[0]//2, shape[1]//2, 3:] = 1.0
+    return x
+
+def make_seed(shape, n_channels):
+    seed = np.zeros([shape[0], shape[1], n_channels], np.float32)
+    seed[shape[0]//2, shape[1]//2, 3:] = 1.0
+    return seed
+
+def make_circle_masks(n, h, w):
+    x = np.linspace(-1.0, 1.0, w)[None, None, :]
+    y = np.linspace(-1.0, 1.0, h)[None, :, None]
+    center = np.random.random([2, n, 1, 1])*1.0-0.5
+    r = np.random.random([n, 1, 1])*0.3+0.1
+    x, y = (x-center[0])/r, (y-center[1])/r
+    mask = (x*x+y*y < 1.0).astype(np.float32)
+    return mask
+
+#CAModel
+class CAModel(nn.Module):
+    def __init__(self, channel_n, fire_rate, device, hidden_size=128):
+        super(CAModel, self).__init__()
+
+        self.device = device
+        self.channel_n = channel_n
+
+        self.fc0 = nn.Linear(channel_n*3, hidden_size)
+        self.fc1 = nn.Linear(hidden_size, channel_n, bias=False)
+        with torch.no_grad():
+            self.fc1.weight.zero_()
+
+        self.fire_rate = fire_rate
+        self.to(self.device)
+
+    def alive(self, x):
+        return F.max_pool2d(x[:, 3:4, :, :], kernel_size=3, stride=1, padding=1) > 0.1
+
+    def perceive(self, x, angle):
+
+        def _perceive_with(x, weight):
+            conv_weights = torch.from_numpy(weight.astype(np.float32)).to(self.device)
+            conv_weights = conv_weights.view(1,1,3,3).repeat(self.channel_n, 1, 1, 1)
+            return F.conv2d(x, conv_weights, padding=1, groups=self.channel_n)
+
+        dx = np.outer([1, 2, 1], [-1, 0, 1]) / 8.0  # Sobel filter
+        dy = dx.T
+        c = np.cos(angle*np.pi/180)
+        s = np.sin(angle*np.pi/180)
+        w1 = c*dx-s*dy
+        w2 = s*dx+c*dy
+
+        y1 = _perceive_with(x, w1)
+        y2 = _perceive_with(x, w2)
+        y = torch.cat((x,y1,y2),1)
+        return y
+
+    def update(self, x, fire_rate, angle):
+        x = x.transpose(1,3)
+        pre_life_mask = self.alive(x)
+
+        dx = self.perceive(x, angle)
+        dx = dx.transpose(1,3)
+        dx = self.fc0(dx)
+        dx = F.relu(dx)
+        dx = self.fc1(dx)
+
+        if fire_rate is None:
+            fire_rate=self.fire_rate
+        stochastic = torch.rand([dx.size(0),dx.size(1),dx.size(2),1])>fire_rate
+        stochastic = stochastic.float().to(self.device)
+        dx = dx * stochastic
+
+        x = x+dx.transpose(1,3)
+
+        post_life_mask = self.alive(x)
+        life_mask = (pre_life_mask & post_life_mask).float()
+        x = x * life_mask
+        return x.transpose(1,3)
+
+    def forward(self, x, steps=1, fire_rate=None, angle=0.0):
+        for step in range(steps):
+            x = self.update(x, fire_rate, angle)
+        return x
 
 def load_emoji(index, path="emojis.png"):
     import imageio
